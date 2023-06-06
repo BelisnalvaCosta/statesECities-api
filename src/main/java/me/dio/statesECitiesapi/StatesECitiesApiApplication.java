9c10f9e76d062a677086eada40f4c4a5a2584791
BelisnalvaCosta <bella-costa@hotmail.com.br>
2023-06-06 17:32:31 -0300

    Commit Initial and branch master

---
+++ src/main/java/me/dio/statesECitiesapi/StatesECitiesApiApplication.java
@@ -0,0 +1,13 @@
+package me.dio.statesECitiesapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StatesECitiesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StatesECitiesApiApplication.class, args);
+	}
+
+}
